d5104a49694fcd9e374d648281fed27d5c4801f6
Soumyajit <soumyajitsinha888@gmail.com>
2024-10-30 00:38:29 +0530

    Updated Dependencies

--- backend/api/services/translate.py
+++ backend/api/services/translate.py
@@ -3,8 +3,13 @@ import os
 
 def translate(key, text, operation):
   command_args = ['string', operation, 'AES', 'CBC', key, text]
-  command = f"java -cp {os.path.dirname(__file__)}\\mule-secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool {' '.join(command_args)}"
-  result = subprocess.run(command.split(), capture_output=True)
+  command_arg_str = ' '.join(command_args)
+  command = 'java -cp assets/mule-secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool ' + (command_arg_str)
+  print('>>>> COMMAND : ' + command)
+  try:
+    result = subprocess.run(command.split(), capture_output=True)
+  except Exception as e: 
+    print('>>>> ERROR : ' + e)
   if result.returncode == 0:
     mod = (result.stdout.decode('utf-8')).strip()
     output = ('![' + mod + ']') if (operation == 'encrypt') else (mod)
