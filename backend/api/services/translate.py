8915c862dcdc8039602430411040af4c573aadba
Soumyajit <soumyajitsinha888@gmail.com>
2024-10-29 23:58:41 +0530

    Updated Dependencies

--- backend/api/services/translate.py
+++ backend/api/services/translate.py
@@ -1,6 +1,19 @@
 import subprocess
+import os
 jar_path = 'assets/mule-secure-properties-tool.jar'
+
 def translate(key, text, operation):
+  directory_path = "assets/"  # Replace with the actual path
+
+  try:
+    for filename in os.listdir(directory_path):
+      filepath = os.path.join(directory_path, filename)
+      print(filepath)
+  except FileNotFoundError:
+    print(f"Directory '{directory_path}' not found.")
+  except OSError as error:
+    print(f"Error accessing directory: {error}")
+
   command_args = ['string', operation, 'AES', 'CBC', key, text]
   command = f"java -cp {jar_path} com.mulesoft.tools.SecurePropertiesTool {' '.join(command_args)}"
   result = subprocess.run(command.split(), capture_output=True)
