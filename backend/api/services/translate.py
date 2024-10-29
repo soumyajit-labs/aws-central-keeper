12a8e9653e6e17944823bd08e09024376d462716
Soumyajit <soumyajitsinha888@gmail.com>
2024-10-30 00:54:37 +0530

    Updated Dependencies

--- backend/api/services/translate.py
+++ backend/api/services/translate.py
@@ -1,11 +1,13 @@
 import subprocess
 import os
+from django.conf import settings
+from django.templatetags.static import static
 
 def translate(key, text, operation):
+  file_path = os.path.join(settings.BASE_DIR, static('mule-secure-properties-tool.jar'))
   command_args = ['string', operation, 'AES', 'CBC', key, text]
   command_arg_str = ' '.join(command_args)
-  command = 'java -cp assets/mule-secure-properties-tool.jar com.mulesoft.tools.SecurePropertiesTool ' + (command_arg_str)
-  print('>>>> COMMAND : ' + command)
+  command = 'java -cp ' + (file_path) + ' com.mulesoft.tools.SecurePropertiesTool ' + (command_arg_str)
   try:
     result = subprocess.run(command.split(), capture_output=True)
   except Exception as e: 
