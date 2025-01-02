3b3ac85a28fbffc97e3bc5203bd80396a4d5e41e
Soumyajit <soumyajitsinha888@gmail.com>
2025-01-02 14:41:07 +0530

    Updates

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,3 +1,4 @@
+import api from "../api";
 import { ACCESS_TOKEN } from "../constants";
 import { useState, useEffect } from "react";
 
@@ -8,7 +9,7 @@ function ProtectedRoute({ children }) {
     useEffect(() => {
         const validateCookies = async () => {
             try {
-                const response = await axios.get("https://sso-gatekeeper.onrender.com/verify", { withCredentials: true });
+                const response = await api.get("https://sso-gatekeeper.onrender.com/verify");
                 if (response.status === 200 && response.data.valid) {
                     localStorage.setItem(ACCESS_TOKEN, response.data.access_token);
                     setIsAuthorized(true);
